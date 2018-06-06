715aa3a5c93026b8710ed105391752d00552791d
Etto Salomons <etto.salomons@gmail.com>
2018-06-06 17:34:41 +0200

    6 juni

---
+++ notebooks/util/logUtil.py
@@ -0,0 +1,31 @@
+import logging
+import time
+
+logger = None
+
+def initLogging():
+    global logger
+    if not logger:
+        filename = 'logs/run_{:s}.log'.format(time.strftime('%Y%m%d-%H%M'))
+        logger = logging.getLogger('logDiz')
+        logger.setLevel(logging.INFO)
+        ch = logging.FileHandler(filename)
+        ch.setLevel(logging.INFO)
+        formatter = logging.Formatter('[%(asctime)s] %(message)s')
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+def LOG(msg):
+    global logger
+    if not logger:
+        initLogging()
+    logger.info(msg)
+    logger.handlers[0].flush()
+    
+
+def DEBUG(msg):
+    global logger
+    if not logger:
+        initLogging()
+    logger.debug(msg)
+    logger.handlers[0].flush()
